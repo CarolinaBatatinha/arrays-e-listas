e3e768cc4b3d9654758d8e8b8f3c419c4f3857e1
Carolina Moreira Batatinha de Souza <batatinha.carolina@gmail.com>
2022-09-08 20:28:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var listaFilmes = [
+    "https://br.web.img3.acsta.net/pictures/14/10/07/22/16/591185.jpg",
+    "https://br.web.img3.acsta.net/pictures/17/12/07/11/33/0502209.jpg",
+    "https://br.web.img2.acsta.net/medias/nmedia/18/90/53/94/20101506.jpg",
+    "https://br.web.img3.acsta.net/pictures/16/09/21/19/14/207893.jpg",
+    "https://br.web.img2.acsta.net/medias/nmedia/18/96/51/73/20472341.jpg",
+    "https://upload.wikimedia.org/wikipedia/pt/1/10/Indiana_Jones_and_the_Temple_of_Doom_PosterB.jpg",
+    "https://www.cineplayers.com/sites/default/files/posters/2020/04/seven_0.jpg",
+    "https://lumiere-a.akamaihd.net/v1/images/unnamed_13_75a3ebb1.jpeg",
+    "https://br.web.img3.acsta.net/medias/nmedia/18/87/87/63/19962446.jpg"
+  ];
+  
+  for (var i = 0; i < listaFilmes.length; i++) {
+    document.write("<img src=" + listaFilmes[i] + ">");
+  }
+  
+  
